a7980b5b1c1a327a2df0081f48aa48952145b0ff
YB912 <YB912TU@gmail.com>
2023-05-23 15:18:08 +0430

    Add Utility class and the .sln file for the library

---
+++ AlgorithmsLib/AlgorithmsLib/Utility.cs
@@ -0,0 +1,9 @@
+﻿
+
+namespace AlgorithmsLib
+{
+    internal class Utility
+    {
+        private void swap(double[] array, int first, int second) { (array[first], array[second]) = (array[second], array[first]); }
+    }
+}

8dd1e73172e727c59404732ce06c4b35fb89ad80
YB912 <YB912TU@gmail.com>
2023-05-23 15:06:57 +0430

    Add binary search

---
+++ AlgorithmsLib/AlgorithmsLib/SearchAlgorithms.cs
@@ -0,0 +1,22 @@
+﻿namespace AlgorithmsDesign
+{
+    public class SearchAlgorithms
+    {
+        public static int BinarySearch(int[] array, int low, int high, double value)
+        {
+            int midPoint = (low + high) / 2;
+            if (value == array[midPoint]) { return midPoint; }
+            else if (value < array[midPoint]) { return BinarySearch(array, low, midPoint, value); }
+            else { return BinarySearch(array, midPoint + 1, high, value); }
+        }
+        public static int BinarySearch(int[] array, double value)
+        {
+            var low = 0;
+            var high = array.Length - 1;
+            var midPoint = (low + high) / 2;
+            if (value == array[midPoint]) { return midPoint; }
+            else if (value < array[midPoint]) { return BinarySearch(array, low, midPoint, value); }
+            else { return BinarySearch(array, midPoint + 1, high, value); }
+        }
+    }
+}

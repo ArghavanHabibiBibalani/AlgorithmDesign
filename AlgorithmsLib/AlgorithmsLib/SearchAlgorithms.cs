31136af2e2ed4e216e0b7f091f77fbc05653010f
YB912 <YB912TU@gmail.com>
2023-05-31 00:56:57 +0330

    Optimize a binary search overload

--- AlgorithmsLib/AlgorithmsLib/SearchAlgorithms.cs
+++ AlgorithmsLib/AlgorithmsLib/SearchAlgorithms.cs
@@ -2,21 +2,13 @@
 {
     public class SearchAlgorithms
     {
-        public static int BinarySearch(int[] array, int low, int high, double value)
+        public static int BinarySearch(int[] array, double value) { return BinarySearch(array, value, 0, array.Length - 1); }
+        public static int BinarySearch(int[] array, double value, int low, int high)
         {
             int midPoint = (low + high) / 2;
             if (value == array[midPoint]) { return midPoint; }
-            else if (value < array[midPoint]) { return BinarySearch(array, low, midPoint, value); }
-            else { return BinarySearch(array, midPoint + 1, high, value); }
-        }
-        public static int BinarySearch(int[] array, double value)
-        {
-            var low = 0;
-            var high = array.Length - 1;
-            var midPoint = (low + high) / 2;
-            if (value == array[midPoint]) { return midPoint; }
-            else if (value < array[midPoint]) { return BinarySearch(array, low, midPoint, value); }
-            else { return BinarySearch(array, midPoint + 1, high, value); }
+            else if (value < array[midPoint]) { return BinarySearch(array, value, low, midPoint); }
+            else { return BinarySearch(array, value, midPoint + 1, high); }
         }
     }
 }

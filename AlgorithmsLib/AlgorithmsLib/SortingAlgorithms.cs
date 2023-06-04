109c15db917cde6837a524cf51481f032aee0e75
arghavan <arghavan.habibibibalani@gmail.com>
2023-06-04 12:49:32 +0330

    Merge Sort

--- AlgorithmsLib/AlgorithmsLib/SortingAlgorithms.cs
+++ AlgorithmsLib/AlgorithmsLib/SortingAlgorithms.cs
@@ -48,5 +48,53 @@ namespace AlgorithmsLib
             }
             return left;
         }
+
+        public static double[] MergeSort(double[] unsorted_Array)
+        {
+            if(unsorted_Array.Length ==0 || unsorted_Array.Length == 1)
+            {
+                return unsorted_Array;
+            }
+            else
+            {
+                double[] left_Array = new double[unsorted_Array.Length / 2];
+                double[] right_Array = new double[unsorted_Array.Length / 2];
+                int middle_Element = unsorted_Array.Length / 2 ;
+
+                for(int i = 0; i < middle_Element; i++)
+                {
+                    left_Array[i] = unsorted_Array[i];
+                }
+                for(int i = middle_Element; i < unsorted_Array.Length; i++)
+                {
+                    right_Array[i - middle_Element] = unsorted_Array[i];
+                }
+
+
+                left_Array = MergeSort(left_Array);
+                right_Array = MergeSort(right_Array);
+                return Merge(left_Array, right_Array);
+            }
+        }
+
+        private static double[] Merge(double[] left_Array, double[] right_Array)
+        {
+            double[] merged = new double[left_Array.Length + right_Array.Length];
+            int i_left = 0;
+            int i_right = 0;
+            int i_merged = 0;
+            while (right_Array.Length < right_Array.Length || left_Array.Length < left_Array.Length)///////////
+            {
+                if (left_Array[i_left] >= right_Array[i_right])
+                {
+                    merged[i_merged++] = left_Array[i_left++];
+                }
+                else
+                {
+                    merged[i_merged++] = right_Array[i_right++];
+                }
+            }
+            return merged;
+        }
     }
 }
--- DebuggingConsole/DebuggingConsole/Program.cs
+++ DebuggingConsole/DebuggingConsole/Program.cs
@@ -2,8 +2,12 @@
 
 public class Program
 {
-    static void Main(string[] args)
+    static void Main(string[] args, SortingAlgorithms sortingAlgorithms)
     {
-        
+        double[] a = new double[] { 27, 10, 12, 20, 25, 13, 15, 22 };
+        //SortingAlgorithms sortingAlgorithms = new SortingAlgorithms();
+        double[] i  = SortingAlgorithms.MergeSort(a);
+        Console.WriteLine(i);
+
     }
 }
